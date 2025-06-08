9367009fdf64b734bc6d908333e1706297adfe0b
isHardCoded <ishardcoded@gmail.com>
2025-06-08 13:05:39 +0300

    Добавили каталог товаров, карточки и стилизацию для них

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,38 @@
-// изменяем импорт
 import styles from './App.module.css'
+import SneakersImage from './assets/images/sneakers-image.jpg'
 
 function Product() {
 	return (
-		<>
-			{/* вытаскиваем класс product из объекта styles */}
-			<p className={styles.product}>Товар</p>
-		</>
+		<div className={styles.product}>
+			<img src={SneakersImage} alt='' />
+			<div className={styles.content}>
+				<div className={styles.title}>
+					<h4>Sneakers Red & White 2025</h4>
+					<button>+</button>
+				</div>
+				<p className={styles.description}>NIKE</p>
+				<p className={styles.price}>$38.00</p>
+			</div>
+		</div>
 	)
 }
 
 function App() {
 	return (
 		<>
-			<h1>Интернет-магазин</h1>
-			<Product />
-			<Product />
-			<Product />
+			<div className={styles.container}>
+				<ul className={styles.list}>
+					<li>
+						<Product />
+					</li>
+					<li>
+						<Product />
+					</li>
+					<li>
+						<Product />
+					</li>
+				</ul>
+			</div>
 		</>
 	)
 }
