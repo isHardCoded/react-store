3daa7a52e89163206a5c86292421c4dd006e0812
isHardCoded <ishardcoded@gmail.com>
2025-07-09 22:25:03 +0300

    Вынесли логику из компонента App в Catalog. Добавили маршрутизацию в main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Catalog from './pages/catalog'
+import Cart from './pages/cart'
 
 createRoot(document.getElementById('root')).render(
-	<StrictMode>
-		<App />
-	</StrictMode>
+	<Router>
+		<Routes>
+			<Route path='/catalog' element={<Catalog />} />
+			<Route path='/cart' element={<Cart />} />
+		</Routes>
+	</Router>
 )
--- src/pages/catalog/index.jsx
+++ src/pages/catalog/index.jsx
@@ -1,5 +1,15 @@
+import styles from './index.module.scss'
+
+import Header from '../../components/header/index'
+import ProductList from '../../components/list/index'
+
 const Catalog = () => {
-	return <></>
+	return (
+		<div className={styles.container}>
+			<Header />
+			<ProductList />
+		</div>
+	)
 }
 
 export default Catalog
