6cfec52fb816d88b0c6f6f3d1a8506b7330971b9
isHardCoded <ishardcoded@gmail.com>
2025-07-14 09:27:35 +0300

    Добавили перенаправление с главной страницы на /catalog

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate, // 1. Импортируем компонент для редиректа
+} from 'react-router-dom'
 
 import Catalog from './pages/catalog'
 import Cart from './pages/cart'
@@ -7,6 +12,8 @@ import Cart from './pages/cart'
 createRoot(document.getElementById('root')).render(
 	<Router>
 		<Routes>
+			{/* 2. Добавляем редирект с корневого пути */}
+			<Route path='/' element={<Navigate to='/catalog' replace />} />
 			<Route path='/catalog' element={<Catalog />} />
 			<Route path='/cart' element={<Cart />} />
 		</Routes>
