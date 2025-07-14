bcffa33ade4e561a7d91b6e4261a070de414b1ae
isHardCoded <ishardcoded@gmail.com>
2025-07-14 16:50:14 +0300

    Fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import {
 	BrowserRouter as Router,
 	Routes,
 	Route,
-	Navigate, // 1. Импортируем компонент для редиректа
+	Navigate,
 } from 'react-router-dom'
 
 import Catalog from './pages/catalog'
@@ -12,7 +12,6 @@ import Cart from './pages/cart'
 createRoot(document.getElementById('root')).render(
 	<Router>
 		<Routes>
-			{/* 2. Добавляем редирект с корневого пути */}
 			<Route path='/' element={<Navigate to='/catalog' replace />} />
 			<Route path='/catalog' element={<Catalog />} />
 			<Route path='/cart' element={<Cart />} />
