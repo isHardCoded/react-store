62daf6927266e278dc7e4961e3e38fa89be91470
isHardCoded <ishardcoded@gmail.com>
2025-07-31 13:24:11 +0300

    Добавили расчет итоговой цены в деталях заказа

--- src/components/OrderDetail.jsx
+++ src/components/OrderDetail.jsx
@@ -1,26 +1,37 @@
 import styles from './OrderDetail.module.scss'
+import { useCart } from '../hooks/useCart'
 
 const OrderDetail = () => {
+	const { cart } = useCart()
+
+	const subtotal = cart.reduce(
+		(acc, item) => acc + item.price * item.quantity,
+		0
+	)
+	const discount = subtotal * 0.2
+	const delivery = 15
+	const total = subtotal - discount + delivery
+
 	return (
 		<div className={styles.detail}>
 			<h2>Order Summary</h2>
 			<div className={styles.stats}>
 				<div>
 					<p>Subtotal</p>
-					<span>$565</span>
+					<span>${subtotal}</span>
 				</div>
 				<div>
 					<p>Discount (-20%)</p>
-					<span>-$113</span>
+					<span>-${discount}</span>
 				</div>
 				<div>
 					<p>Delivery Fee</p>
-					<span>$15</span>
+					<span>${delivery}</span>
 				</div>
 			</div>
 			<div className={styles.total}>
 				<p>Total</p>
-				<span>$467</span>
+				<span>${total}</span>
 			</div>
 			<button>Go to Checkout</button>
 		</div>
